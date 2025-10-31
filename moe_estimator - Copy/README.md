ede4b8c1cf5435adea76812a6e264b586eee709d
Odera <34449885+oderao@users.noreply.github.com>
2025-10-31 22:51:41 +0100

    Add files via upload

---
+++ moe_estimator - Copy/README.md
@@ -0,0 +1,5 @@
+# Moe Estimator
+
+Frappe/ERPNext app for millwork estimating and materials.
+
+**Upload steps** are in `UPLOAD_STEPS.md`.
---
+++ moe_estimator - Copy/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="moe_estimator",
+    version="0.0.1",
+    description="Millwork Estimator",
+    author="ACF",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
